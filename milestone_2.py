362690e5979df64874d081568ddf1ba8945d06c1
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-11-29 23:47:18 +0000

    Add if statement

--- milestone_2.py
+++ milestone_2.py
@@ -8,3 +8,9 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Enter a single letter: ")
+
+
+if len(guess) == 1 and guess.isdigit() == False:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
