14e29f96bb0c6a9b7a9904d0544d667c04ff89fc
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-11-30 08:17:08 +0000

    Add check if guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,17 @@
+import random
+
+word_list = ["apple", "kiwi", "banana", "watermelon", "strawberry"]
+
+word = random.choice(word_list)
+
 while True:
     guess = input("Guess a single letter: ")
     if len(guess) == 1 and guess.isdigit() == False:
         break
     else: 
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
