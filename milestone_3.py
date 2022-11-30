9fcdc4379cb2c9aa7feb1c444e5e2695ef6f682f
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-11-30 16:09:43 +0000

    Add functions in the file

--- milestone_3.py
+++ milestone_3.py
@@ -4,14 +4,31 @@ word_list = ["apple", "kiwi", "banana", "watermelon", "strawberry"]
 
 word = random.choice(word_list)
 
-while True:
-    guess = input("Guess a single letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else: 
-        print("Invalid letter. Please, enter a single alphabetical character.")
+print(word)
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+    
+
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a single letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else: 
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
+
+
+
+
+    
+    
+
