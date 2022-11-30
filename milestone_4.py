6fea066391d274ba8287cbd685b4be0320e5c5e7
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-11-30 16:52:33 +0000

    Add num_lives parameter into __init__

--- milestone_4.py
+++ milestone_4.py
@@ -3,9 +3,9 @@ import random
 
 class Hangman():
 
-    def __init__(self, word_list):
+    def __init__(self, word_list, num_lives=5):
         self.word_list:list = word_list
-        self.num_lives:int = 5
+        self.num_lives:int = num_lives
         self.word:str = random.choice(self.word_list)
         self.word_guessed:list = ["_" for _ in range(0,len(self.word))]
         self.num_letters:int = len(self.word)
